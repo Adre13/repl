c2c3027cf3fc0264b79ac526ac3f8d1cb9d631f4
max <tolstyu@gmail.com>
2018-08-23 15:01:41 +0300

    the sum

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println(a+b);
+        String str1;
+        str1 = in.nextLine();
+        if(str1.equals("")){
+            System.out.println("\n");
+        }else if(str1.equals("/exit")){
+            System.out.println("Bye!");
+        }else{
+            String[] temp = str1.split(" ");
+            if(temp.length == 1){
+                System.out.println(Integer.parseInt(temp[0]));
+            }else{
+                System.out.println(Integer.parseInt(temp[0]) + Integer.parseInt(temp[1]));
+            }
+        }
     }
 
 }
